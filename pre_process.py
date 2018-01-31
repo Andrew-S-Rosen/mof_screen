fa8a5bcde1064cf132e0721300c652f8f127bc35
Andrew S. Rosen <rosen@u.northwestern.edu>
2018-01-31 17:08:56 -0600

    Update pre_process.py

--- pre_process.py
+++ pre_process.py
@@ -19,24 +19,32 @@ for filename in os.listdir(mofpath):
 			print('No C in MOF: '+refcode)
 			continue
 		stoichs.append(mof.get_chemical_formula())
-os.remove(mofpath+'POSCAR')
 unique_id,id_counts = np.unique(stoichs,return_counts=True)
 mult_ids = unique_id[id_counts > 1].tolist()
+stoichs = np.array(stoichs)
+refcodes = np.array(refcodes)
 for mult_id in mult_ids:
 	position_mats = []
 	idx = []
 	for i, stoich in enumerate(stoichs):
 		if stoich == mult_id:
-			parser = CifParser(mofpath+refcode+'.cif')
+			parser = CifParser(mofpath+refcodes[i]+'.cif')
 			pm_mof = parser.get_structures(primitive=True)[0]
 			pm_mof.to(filename='POSCAR')
 			mof = read('POSCAR')
 			position_mats.append(mof.get_positions())
 			idx.append(i)
+	idx = np.array(idx)
 	position_mats = np.array(position_mats)
 	n = np.shape(position_mats)[0]
 	for i in range(n):
-		diffs = (np.sum((position_mats[i,:,:]-position_mats[np.arange(n) != i,:,:])**2,axis=1)/n)**(0.5)
-		for j in range(n-1):
-			if all(diffs[j,:] < 0.1):
-				print('WARNING: '+refcodes[idx[i]]+' ('+stoichs[idx[i]]+') is a duplicate with '+refcodes[idx[j]]+' ('+stoichs[idx[j]]+')')
+		j_vec = np.setdiff1d(np.arange(n),i)
+		dups = []
+		for j in j_vec:
+			diff = (np.sum((position_mats[i,:,:]-position_mats[j,:,:])**2)/n)**(0.5)
+			if diff < 0.1:
+				dups.append(j)
+		if dups:
+			print(refcodes[idx[i]]+' ('+stoichs[idx[i]]+') is a duplicate of '+str(refcodes[idx[dups]]))
+	print('')
+os.remove('POSCAR')
