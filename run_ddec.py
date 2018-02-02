7f2e87b43756157f85bd66c2e68170bd2a67bdc2
Andrew S. Rosen <rosen@u.northwestern.edu>
2018-02-02 15:38:11 -0600

    Update run_ddec.py

--- run_ddec.py
+++ run_ddec.py
@@ -1,11 +1,12 @@
 import os
 from shutil import copyfile
 
-results_path = '/projects/p30148/vasp_jobs/MOFs/reoptimized_core1/results/AVEMOE_clean_min/test/'
+results_path = '/projects/p30148/vasp_jobs/MOFs/reoptimized_core1/results/'
 job_control_path = '/home/asr731/software/chargemol_09_26_2017/scripts/job_control.txt'
 submit_script_path = '/home/asr731/software/chargemol_09_26_2017/scripts/sub_ddec.job'
 sub_command = 'msub'
 for refcode in os.listdir(results_path):
+	print(refcode)
 	spe_path = results_path+refcode+'/final_spe/'
 	if os.path.isdir(spe_path):
 		for subdir in os.listdir(spe_path):
@@ -17,4 +18,5 @@ for refcode in os.listdir(results_path):
 				copyfile(spe_path+subdir+'/'+file,ddec_path+file)
 			copyfile(job_control_path,ddec_path+'job_control.txt')
 			copyfile(submit_script_path,ddec_path+'sub_ddec.job')
-			os.system(sub_command+' '+ddec_path+'sub_ddec.job')
+			os.chdir(ddec_path)
+			os.system(sub_command +' sub_ddec.job')
