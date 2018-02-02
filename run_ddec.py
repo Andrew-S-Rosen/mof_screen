33a65405814b69490e4bc26c35db35ed05434fe8
Andrew S. Rosen <rosen@u.northwestern.edu>
2018-02-02 15:54:23 -0600

    Update run_ddec.py

--- run_ddec.py
+++ run_ddec.py
@@ -5,10 +5,12 @@ results_path = '/projects/p30148/vasp_jobs/MOFs/reoptimized_core1/results/'
 job_control_path = '/home/asr731/software/chargemol_09_26_2017/scripts/job_control.txt'
 submit_script_path = '/home/asr731/software/chargemol_09_26_2017/scripts/sub_ddec.job'
 sub_command = 'msub'
-for refcode in os.listdir(results_path):
-	print(refcode)
+refcodes = os.listdir(results_path)
+refcodes.sort()
+for refcode in refcodes:
 	spe_path = results_path+refcode+'/final_spe/'
 	if os.path.isdir(spe_path):
+		print(refcode)
 		for subdir in os.listdir(spe_path):
 			ddec_path = spe_path+subdir+'/ddec/'
 			if not os.path.exists(ddec_path):
