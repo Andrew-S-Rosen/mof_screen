1075f92cc6a21a5dd3da2a04b9ca57fd8543f41b
Andrew S. Rosen <rosen@u.northwestern.edu>
2018-03-10 15:31:03 -0600

    Update get_oms.py

--- utilities/oms/get_oms.py
+++ utilities/oms/get_oms.py
@@ -1,9 +1,8 @@
 import os
 mof_path = '/projects/p30148/vasp_jobs/MOFs/reoptimized_core1/results_cifs/reoptimized_cifs/'
 zeo_path = '/home/asr731/software/zeo++-0.3/network'
-os.chdir(mof_path)
 cif_files = []
 for filename in os.listdir(mof_path):
 	filename = filename.strip()
 	if '.cif' in filename:
-		os.system(zeo_path+' -omsex '+filename)
+		os.system(zeo_path+' -omsex '+mof_path+filename)
