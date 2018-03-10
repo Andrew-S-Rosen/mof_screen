2c9003af87eb81e08056fee33c3e0d173fc5e303
Andrew S. Rosen <rosen@u.northwestern.edu>
2018-03-10 10:59:28 -0600

    Use VIRE NN approach

--- utilities/ads_checkers/ads_check.py
+++ utilities/ads_checkers/ads_check.py
@@ -1,24 +1,28 @@
 import pymatgen as pm
+from pymatgen.analysis.local_env import MinimumVIRENN
+import numpy as np
 import os
+
+#make sure it's bound to metal type
 results_path = '/projects/p30148/vasp_jobs/MOFs/oxidized_oms/results/'
-rcut = 2.5
 ads_species = 'O'
-nonmetals_list = ['H','He','C','N','O','O','F','Ne','P','S','Cl','Ar','Se','Br','Kr','I','Xe','Rn']
+nonmetals_list = ['H','He','C','N','O','F','Ne','P','S','Cl','Ar','Se','Br','Kr','I','Xe','Rn']
+bad_jobs = []
 for refcode in os.listdir(results_path):
 	spe_path = results_path+refcode+'/final_spe/'
 	if os.path.isdir(spe_path):
 		for subdir in os.listdir(spe_path):
+			dist = []
+			full_name = refcode+'_'+subdir
 			contcar_path = spe_path+subdir+'/CONTCAR'
-			struct = pm.Structure.from_file(contcar_path)
-			main_site = [atom for atom in struct if atom.species_string == ads_species][-1]
-			neighbors = struct.get_neighbors(main_site,rcut)
-			if len(neighbors) == 0:
-				print(refcode+'_'+subdir)
-			else:
-				neighbors_strs = [specie[0].species_string for specie in neighbors]
-				dist = [specie[1] for specie in neighbors]
-				orig_env = neighbors_strs+dist
-				env = list(zip(neighbors_strs,dist))
-				env.sort(key=lambda x: x[1])
-				if env[0][0] in nonmetals_list:
-					print(refcode+'_'+subdir)
+			struct = pm.Structure.from_file(contcar_path,primitive=False,sort=False)
+			nn_object = MinimumVIRENN()
+			ads_idx = [i for i, atom in enumerate(struct) if atom.species_string == ads_species][-1]
+			neighbors = nn_object.get_nn_info(struct,ads_idx)
+			if neighbors:
+				for neighbor in neighbors:
+					dist.append(struct.get_distance(ads_idx,neighbor['site_index']))
+				if neighbors[np.argmin(dist)]['site'].species_string in nonmetals_list:
+					bad_jobs.append(full_name)
+bad_jobs.sort()
+print(bad_jobs)
