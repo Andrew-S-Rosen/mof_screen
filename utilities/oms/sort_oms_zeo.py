8cbd06cd42b626ffe4fd6c13d973855c5f6da2f8
Andrew S. Rosen <rosen@u.northwestern.edu>
2018-03-10 15:30:20 -0600

    Update sort_oms_zeo.py

--- utilities/oms/sort_oms_zeo.py
+++ utilities/oms/sort_oms_zeo.py
@@ -3,12 +3,10 @@ from shutil import copyfile
 mof_path = '/projects/p30148/vasp_jobs/MOFs/reoptimized_core1/results_cifs/reoptimized_cifs/'
 omsdata = '/projects/p30148/vasp_jobs/MOFs/reoptimized_core1/results_cifs/reoptimized_cifs/OMS_data/'
 newmofpath = '/projects/p30148/vasp_jobs/MOFs/reoptimized_core1/results_cifs/reoptimized_oms_cifs/'
-os.chdir(mof_path)
 cif_files = []
 for filename in os.listdir(mof_path):
 	filename = filename.strip()
 	refcode = filename.split('.cif')[0]
-	if len(filename.split('.cif')) == 2:
-		f = open(omsdata+refcode+'.omsex')
-		if len(f.read()) != 0:
-			copyfile(filename,newmofpath+filename)
+	if '.cif' in filename:
+		if os.stat(omsdata+refcode+'.omsex').st_size > 0:
+			copyfile(mof_path+filename,newmofpath+filename)
