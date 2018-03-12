4131dbfbd95953fb303d85e37b397a736edf370c
Andrew S. Rosen <rosen@u.northwestern.edu>
2018-03-12 00:06:47 -0500

    Don't rerun jobs that finished

--- utilities/charges/run_bader.py
+++ utilities/charges/run_bader.py
@@ -2,7 +2,7 @@ import os
 from shutil import copyfile
 
 results_path = '/projects/p30148/vasp_jobs/MOFs/reoptimized_core1/results/'
-submit_script_path = '/home/asr731/software/chargemol_09_26_2017/scripts/sub_bader.job'
+submit_script_path = '/home/asr731/bin/submit_scripts/sub_bader.job'
 sub_command = 'msub'
 refcodes = os.listdir(results_path)
 refcodes.sort()
@@ -14,6 +14,9 @@ for refcode in refcodes:
 			bader_path = spe_path+subdir+'/bader/'
 			if not os.path.exists(bader_path):
 				os.makedirs(bader_path)
+			bader_path_files = os.listdir(bader_path)
+			if 'ACF.dat' in bader_path_files and 'BCF.dat' in bader_path_files:
+				continue
 			files = ['AECCAR0','AECCAR2','CHGCAR']
 			for file in files:
 				copyfile(spe_path+subdir+'/'+file,bader_path+file)
