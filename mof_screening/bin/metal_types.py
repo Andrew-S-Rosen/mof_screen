90975935b7e42624968a5ef947cbc33bfb2c9105
Andrew S. Rosen <rosen@u.northwestern.edu>
2018-03-23 09:05:07 -0500

    Name change

--- mof_screening/bin/metal_types.py
+++ mof_screening/bin/metal_types.py
@@ -1,9 +1,9 @@
 import numpy as np
-sblock_metals = [3,4,11,12,19,20,37,38,55,56,87,88]
+spblock_metals = [3,4,11,12,19,20,37,38,55,56,87,88]
 dblock_metals = np.concatenate((np.arange(21,30,1),np.arange(39,48,1),np.arange(71,80,1),np.arange(103,112,1)),axis=0).tolist()
 fblock_metals = np.concatenate((np.arange(57,71,1),np.arange(89,103,1)),axis=0).tolist()
 nonmetal_list = [1,2,6,7,8,9,10,15,16,17,18,34,35,36,53,54,86]
 metal_list = [val for val in np.arange(1,119,1) if val not in nonmetal_list]
-mag_list = [metal for metal in metal_list if metal not in sblock_metals]
+mag_list = [metal for metal in metal_list if metal not in spblock_metals]
 nomag_list = [val for val in np.arange(1,119,1) if val not in mag_list]
-poor_metals = [metal for metal in metal_list if metal not in dblock_metals+fblock_metals+sblock_metals]
+poor_metals = [metal for metal in metal_list if metal not in dblock_metals+fblock_metals+spblock_metals]
