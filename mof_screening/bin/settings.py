1abd571e9e28900bcbaa8afdea53daea07950d1d
Andrew Rosen <rosen@u.northwestern.edu>
2018-03-23 18:00:46 -0500

    Update default settings

--- mof_screening/bin/settings.py
+++ mof_screening/bin/settings.py
@@ -1,8 +1,7 @@
 #PATHS
 kpts_path = '/projects/p30148/vasp_jobs/MOFs/reoptimized_core1/results_cleaned/data_for_phase2/kpts.txt'
-#mofpath = '/projects/p30148/vasp_jobs/MOFs/oxidized_oms/results_cleaned/data_for_phase3/'
-mofpath = '/projects/p30148/vasp_jobs/MOFs/testing/runners/here/'
-basepath = '/projects/p30148/vasp_jobs/MOFs/testing/'
+mofpath = '/projects/p30148/vasp_jobs/MOFs/oxidized_oms/results_cleaned/data_for_phase3/'
+basepath = '/projects/p30148/vasp_jobs/MOFs/phase3/runner/runner1/'
 
 #CALCULATION DETAILS
 ads_species = 'O' #adsorbate species
