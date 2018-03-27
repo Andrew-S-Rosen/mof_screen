5954904602127da75c46e9b210d775dcd975b5e2
Andrew S. Rosen <rosen@u.northwestern.edu>
2018-03-27 13:36:17 -0500

    Update screen.py

--- mof_screening/screen.py
+++ mof_screening/screen.py
@@ -97,7 +97,7 @@ def run_ads_screen(cif_files):
 					mof = read(spin1_final_mof_path)
 				else:
 					mof = cif_to_mof(cif_file)
-				manage_restart_files(results_partial_paths[run_i-1]+'/'+spin_level)s
+				manage_restart_files(results_partial_paths[run_i-1]+'/'+spin_level)
 				mof = set_initial_magmoms(mof,spin_level)
 				gpt_version, nprocs = get_gpt_version(kpts_lo,len(mof),nprocs,ppn)
 				choose_vasp_version(gpt_version,nprocs,calc_swaps)
