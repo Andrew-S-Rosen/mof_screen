a126af2eac53800f556a6faa19ca79fc1fed2ff4
Andrew Rosen <rosen@u.northwestern.edu>
2018-03-30 10:38:53 -0500

    Finally fix force issues....

--- mof_screening/screen.py
+++ mof_screening/screen.py
@@ -236,7 +236,8 @@ def run_ads_screen(cif_files):
 				pprint('Skipping rest because of errors')
 				break
 			success_path = basepath+'results/'+refcode+'/'+acc_level+'/'+spin_level
-			if np.max(np.linalg.norm(read(success_path+'/OUTCAR').get_forces(),axis=1)) > f_tol:
+			f_final = np.max(np.linalg.norm(read(success_path+'/OUTCAR').get_forces(),axis=1))
+			if f_final > f_tol:
 				pprint('WARNING: |F_max| = '+str(f_tol))
 				
 			#***********SAVE and CONTINUE***********
@@ -526,7 +527,8 @@ def run_vol_screen(cif_files):
 				pprint('Skipping rest because of errors')
 				break
 			success_path = basepath+'results/'+refcode+'/'+acc_level+'/'+spin_level
-			if np.max(np.linalg.norm(read(success_path+'/OUTCAR').get_forces(),axis=1)) > f_tol:
+			f_final = np.max(np.linalg.norm(read(success_path+'/OUTCAR').get_forces(),axis=1))
+			if f_final > f_tol:
 				pprint('WARNING: |F_max| = '+str(f_tol))
 
 			#***********SAVE and CONTINUE***********
