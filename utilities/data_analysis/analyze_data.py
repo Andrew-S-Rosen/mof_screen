de95dbf7bed3450e2558bc11c16a8676e9bb0c8a
Andrew Rosen <rosen@u.northwestern.edu>
2018-04-05 22:44:31 -0500

    Update script for more data

--- utilities/data_analysis/analyze_data.py
+++ utilities/data_analysis/analyze_data.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 from ase.geometry import cell_to_cellpar
 import matplotlib.pyplot as plt
+
 phase1_results = '/projects/p30470/phase1_results/'
 phase2_results = '/projects/p30148/vasp_jobs/MOFs/oxidized_oms/results/'
 phase3_results = '/projects/p30148/vasp_jobs/MOFs/phase3/results/'
@@ -37,6 +38,28 @@ def get_gas_dict(gas_path):
 	gas_dict = {'H2O':E_H2O,'O2':E_O2,'CH4':E_CH4}
 	return gas_dict
 
+def get_NNs(screen_path,mof_name):
+	try:
+		with open(screen_path+'NN_list.txt','r') as rf:
+			for line in rf:
+				if line.split('|')[0] == mof_name:
+					NNs = line.split('|')[-1].strip()
+					break
+	except:
+		NNs = np.nan
+	return NNs
+
+def check_bad(screen_path,mof_name):
+	isbad = False
+	try:
+		with open(screen_path+'bad_ads_addition.txt','r') as rf:
+			for line in rf:
+				if line.strip() == mof_name:
+					isbad = True
+	except:
+		pass
+	return isbad
+
 def get_mof_df(screen_path):
 	refcode_list = []
 	E_list = []
@@ -46,13 +69,13 @@ def get_mof_df(screen_path):
 	V_list = []
 	lattice_list = []
 	formula_list = []
+	NNs_list = []
 	folder_list = os.listdir(screen_path)
 	folder_list.sort()
 	for folder in folder_list:
 		if not os.path.exists(screen_path+'/'+folder+'/final_spe/'):
 			continue
 		refcode = folder
-		refcode_list.append(refcode)
 		E = np.inf
 		for spin_result in os.listdir(screen_path+'/'+folder+'/final_spe/'):
 			mof_path_temp = screen_path+folder+'/final_spe/'+spin_result
@@ -61,7 +84,12 @@ def get_mof_df(screen_path):
 			if E_temp < E:
 				E = E_temp
 				mof_path = mof_path_temp
+		mof_name = folder+'_'+spin_result
+		isbad = check_bad(screen_path,mof_name)
+		if isbad == True:
+			continue
 		mof = read(mof_path+'/OUTCAR')
+		NNs = get_NNs(screen_path,mof_name)
 		try:
 			net_magmom = mof.get_magnetic_moment()
 		except:
@@ -71,6 +99,7 @@ def get_mof_df(screen_path):
 		lattice = cell_to_cellpar(mof.cell)
 		formula = mof.get_chemical_formula()
 
+		refcode_list.append(refcode)
 		kpts_list.append(kpts)
 		gamma_list.append(gamma)
 		E_list.append(E)
@@ -78,8 +107,19 @@ def get_mof_df(screen_path):
 		V_list.append(V)
 		lattice_list.append(lattice)
 		formula_list.append(formula)
-	mof_df = pd.DataFrame({'Name':refcode_list,'Formula':formula_list,'E':E_list,'Mag':net_magmom_list,'Cell':lattice_list,'V':V_list,'kpts':kpts_list,'gamma':gamma_list})
+		NNs_list.append(NNs)
+	mof_df = pd.DataFrame({'Name':refcode_list,'E':E_list,'Formula':formula_list,'NNs':NNs_list,'Mag':net_magmom_list,'Cell':lattice_list,'V':V_list,'kpts':kpts_list,'gamma':gamma_list})
 	return mof_df
+
+def get_NN_list(screen_path):
+	names = []
+	NNs = []
+	with open(screen_path+'NN_list.txt','r') as rf:
+		for line in rf:
+			split_line = line.split('|').strip()
+			names.append(split_line[0])
+			NNs.append(split_line[1])
+	return names, NNs
 
 def plot_descriptor(E_H_list,E_TS_list):
 	plt.figure()
@@ -94,13 +134,25 @@ def write_descriptor(refcodes,E_H_list,E_TS_list,Ea_list):
 		for i,refcode in enumerate(refcodes):
 			wf.write(refcode+','+str(E_H_list[i])+','+str(E_TS_list[i])+','+str(Ea_list[i])+'\n')
 
-def get_df(df_name,df_path):
-	if os.path.exists(df_name+'.pkl'):
-		mof_df = pd.read_pickle(df_name+'.pkl')
+def get_df(phase,df_path):
+	pckl_name = 'phase'+str(phase)+'_df.pkl'
+	if os.path.exists(pckl_name):
+		mof_df = pd.read_pickle(pckl_name)
 	else:
 		mof_df = get_mof_df(df_path)
-		mof_df.to_pickle(df_name+'.pckl')
+		mof_df.to_pickle(pckl_name)
 	return mof_df
+
+def clean_df(phase2_df,phase3_df):
+	for i, row in phase3_df.iterrows():
+		phase3_NNs = row.NNs
+		phase3_name = row.Name
+		phase2_name = phase3_name.rsplit('_spin',1)[0]
+		phase2_NNs = phase2_df[phase2_df['Name'].str.match(phase2_name)].NNs.tolist()[0]
+		if phase2_NNs != phase3_NNs:
+			phase3_df = phase3_df[phase3_df.Name != phase3_name]
+			phase2_df = phase2_df[phase2_df.Name != phase2_name]
+	return phase2_df, phase3_df
 
 def get_E_TS(phase2_df,phase3_df,gas_dict):
 	E_TS_list = []
@@ -108,20 +160,18 @@ def get_E_TS(phase2_df,phase3_df,gas_dict):
 	Ea_list = []
 	phase2_names = []
 	H_ref = 0.5*gas_dict['H2O']-0.25*gas_dict['O2']
-	C_ref = gas_dict['CH4']-4*H_ref
 	for i, row in phase3_df.iterrows():
 		try:
 			phase3_E = row.E
 			phase3_name = row.Name
 			phase2_name = phase3_name.rsplit('_spin',1)[0]
 			phase2_E = float(phase2_df[phase2_df['Name'].str.match(phase2_name)].E)
-			E_ref = phase2_E+4*H_ref+C_ref
 			E_H = phase3_E-phase2_E-H_ref
 			E_TS = 0.75*E_H+1.09
 			E_H_list.append(E_H)
 			E_TS_list.append(E_TS)
 			phase4_E = float(phase4_df[phase4_df['Name'].str.match(phase2_name)].E)
-			Ea = E_TS - (phase4_E - E_ref)
+			Ea = E_TS + phase2_E + gas_dict['CH4'] - phase4_E
 			Ea_list.append(Ea)
 			phase2_names.append(phase2_name)
 		except:
@@ -132,13 +182,16 @@ def get_E_ads(phase2_df,phase4_df,gas_dict):
 	E_ads_list = []
 	phase2_names = []
 	for i, row in phase4_df.iterrows():
-		phase4_E = row.E
-		phase4_name = row.Name
-		phase2_name = phase4_name.rsplit('_spin',1)[0]
-		phase2_E = float(phase2_df[phase2_df['Name'].str.match(phase2_name)].E)
-		E_ads = phase4_E - (phase2_E + gas_dict['CH4'])
-		E_ads_list.append(E_ads)
-		phase2_names.append(phase2_name)
+		try:
+			phase4_E = row.E
+			phase4_name = row.Name
+			phase2_name = phase4_name.rsplit('_spin',1)[0]
+			phase2_E = float(phase2_df[phase2_df['Name'].str.match(phase2_name)].E)
+			E_ads = phase4_E - (phase2_E + gas_dict['CH4'])
+			E_ads_list.append(E_ads)
+			phase2_names.append(phase2_name)
+		except:
+			pass
 	return phase2_names, E_ads_list
 
 def get_E_ox(phase1_df,phase2_df,gas_dict):
@@ -155,12 +208,13 @@ def get_E_ox(phase1_df,phase2_df,gas_dict):
 	return phase1_names, E_ox_list
 
 gas_dict = get_gas_dict(gas_path)
-phase1_df = get_df('phase1_df',phase1_results)
-phase2_df = get_df('phase2_df',phase2_results)
-phase3_df = get_df('phase3_df',phase3_results)
-phase4_df = get_df('phase4_df',phase4_results)
+phase1_df = get_df(1,phase1_results)
+phase2_df = get_df(2,phase2_results)
+phase3_df = get_df(3,phase3_results)
+phase4_df = get_df(4,phase4_results)
+phase2_df, phase3_df = clean_df(phase2_df,phase3_df)
 phase2_TS_names, E_H_list, E_TS_list, Ea_list = get_E_TS(phase2_df,phase3_df,gas_dict)
+plot_descriptor(E_H_list,E_TS_list)
 phase2_ads_names, E_ads_list = get_E_ads(phase2_df,phase4_df,gas_dict)
 phase1_ox_names, E_ox_list = get_E_ox(phase1_df,phase2_df,gas_dict)
 write_descriptor(phase2_TS_names,E_H_list,E_TS_list,Ea_list)
-plot_descriptor(E_H_list,E_TS_list)
